bc43671c37aebf52d69cfbae84449ea1e18f3287
velocichaptor <jake.m.chappelle@gmail.com>
2021-04-19 12:21:18 -0500

    installed faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-users
-- name
-- age
+# users
+# - name
+# - age
 
-locations
-- name
+# locations
+# - name
 
-vacation
-- t.string :title
-- t.datetime :date
-- t.datetime :day
-- t.string :note
-- t.integer :userId
-- t.integer :locationId
+# vacation
+# - t.string :title
+# - t.datetime :date
+# - t.datetime :day
+# - t.string :note
+# - t.integer :userId
+# - t.integer :locationId
+
+# days
+# -t.string :name
+# t.string :forecast
+# t.string :activity
+# t.integer :vacation_id
